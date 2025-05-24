9175ec7ccb5293bb4bd3481655a818bcc3332c3a
MBNascimento <124750651+MBNascimento@users.noreply.github.com>
2025-05-24 14:15:46 -0300

    atualizações

---
+++ montanha _russa_cricri1.py
@@ -0,0 +1,14 @@
+print("")
+idade = 19
+altura = 1.6
+ticket_valido = True
+if ticket_valido:
+    print("Seu ticket é válido.")
+elif idade > 18:
+    print("Você é maior de idade.")
+elif altura > 1.5:
+    print("Altura permitido.")
+else:
+    print("Requisitos não atendidos.")
+
+print(" ")
---
+++ montanha_russa_cricri2.py
@@ -0,0 +1,13 @@
+print(" ")
+
+idade = 19
+altura = 1.6
+ticket_valido = True
+if ticket_valido:
+    print("Seu ticket é válido")
+    if idade > 18 and altura > 1.5:
+        print("Entrada permitida")
+else:
+    print("Requisitos não atendidos")
+
+print("")
