189a9a7077a061e784a37f1d242a05b12beb5e5c
MBNascimento <124750651+MBNascimento@users.noreply.github.com>
2025-07-03 22:41:58 -0300

    Criar while_loop.py

---
+++ whille_loop.py
@@ -0,0 +1,13 @@
+#-*-coding:utf8;-*-
+#qpy:console
+#while loop.py
+print("")
+print( "This is console module")
+print("")
+
+x = 0
+while x <11:
+    print((x),end=" ")
+    x += 1
+print ("")
+print ("1 2 3 4 5 6 7 8 9 10")
